bb5af3afe842fef9479c93d973af70b4c101c552
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-02-10 18:07:22 +0530

    Created Route for Signup

--- app/Users/Authentication/signup.py
+++ app/Users/Authentication/signup.py
@@ -1,8 +1,31 @@
-from app import app
+from app import app, db, migrate, SQLAlchemyError
+from app.models.Authentication import UserModel
 from flask import request
 
-@app.route("/signup", methods = ["POST"])
+@app.route("/signup", methods = ["POST", "GET"])
 def signUp():
-    user = request.json
     
-    return user
+    try:
+        if(request.method == "POST"):
+            print()
+            print()
+            print()
+            print()
+            user = request.json
+            name = user["name"]
+            email = user["email"]
+            password = user["password"]
+            id = 1
+
+            newUser = UserModel(id=1,name=name, email=email, password=password)
+            db.session.add(newUser)
+            data = db.session.commit()
+            if(data):
+                print(data)
+
+    except SQLAlchemyError as e :
+
+        #    error = str(e.__dict__['orig'])
+           print(e)
+    
+    return "Hello world"
